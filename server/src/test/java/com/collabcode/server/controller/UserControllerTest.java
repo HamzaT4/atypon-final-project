1d76e1532135391c16992f548fe57ddd2ef06a9c
hamzat4 <hamzatay2012@gmail.com>
2025-04-23 00:10:38 +0300

    edit tests again

--- server/src/test/java/com/collabcode/server/controller/UserControllerTest.java
+++ server/src/test/java/com/collabcode/server/controller/UserControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
